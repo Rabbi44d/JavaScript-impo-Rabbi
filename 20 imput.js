6bf4c994bbbcda70d8726c7ed16b27034109b752
Rabbi44d <rb15968732@gmail.com>
2024-02-05 16:08:29 +0600

    rabbi 20 20 20

--- 20 imput.js
+++ 20 imput.js
@@ -1,142 +1,142 @@
 // ----------------------------------------------------------------------
 //***** ফংশন বিষয় আলোচনা বর্গ করার নিয়ম বর্গ সুদু এক ভাবে করা যায় ***video--20-1,2***//
 
-function Rabbi(nambar){
-    console.log('This is a heading', nambar)
-    const borgo = nambar * nambar ;
-    console.log('this is a paragraph.', borgo)
-}
-Rabbi(4);
-Rabbi(50);
+// function Rabbi(nambar){
+//     console.log('This is a heading', nambar)
+//     const borgo = nambar * nambar ;
+//     console.log('this is a paragraph.', borgo)
+// }
+// Rabbi(4);
+// Rabbi(50);
 
 // ***আবার যোগ করার বিষয় হল **
 
-function add(Nam1, Nam2, Nam3){
-    const total = Nam1 + Nam2 + Nam3;
-    console.log(total)
-}
-add(50,25,25);
+// function add(Nam1, Nam2, Nam3){
+//     const total = Nam1 + Nam2 + Nam3;
+//     console.log(total)
+// }
+// add(50,25,25);
 
-function Rabbi(a, b, c, d, f, g, h){
-    const mot =a + b + c + d + f + g + h;
-    console.log('This is a website in bangladesh', mot)
+// function Rabbi(a, b, c, d, f, g, h){
+//     const mot =a + b + c + d + f + g + h;
+//     console.log('This is a website in bangladesh', mot)
 
-}
-Rabbi(10, 20, 20, 100, 700, 50, 100);
+// }
+// Rabbi(10, 20, 20, 100, 700, 50, 100);
 
 // ----------------------------------------------------------------------------------------;
 
 //*** বিয়োগ করার বিষয় ফাংশন দিয়ে করে তার বিষয় আলোচনা বা মান আকারে দিতে পারি বা const dite pari ***video--20.3 ***//
 
-function Rabbi(Ra1, Ra2){
-    const Rafi = Ra1 - Ra2;
-    console.log('This is a ba jekono bisoy', Rafi)
-}
-const Najim =50;
-const Nahid =20;
-Rabbi(Najim,Nahid);
+// function Rabbi(Ra1, Ra2){
+//     const Rafi = Ra1 - Ra2;
+//     console.log('This is a ba jekono bisoy', Rafi)
+// }
+// const Najim =50;
+// const Nahid =20;
+// Rabbi(Najim,Nahid);
 
 // -------------------------------------------------------------------------------------;
 
 //**** গুন করার বিষয় ফাংশন নিয়ে আলোচনা ***video--20.4***///
 
-function Rabbi(nam){
-    const Rakib = nam * 20;
-    return Rakib;
-}
+// function Rabbi(nam){
+//     const Rakib = nam * 20;
+//     return Rakib;
+// }
 
-const out = Rabbi(100);
-console.log('this is a heading', out);
+// const out = Rabbi(100);
+// console.log('this is a heading', out);
 
 // --------------------------------------------------------------------------------------------------------;
 
 // *****যোগ বিয়োগ গুন ভাগ একবারে করতে পারি তার হিসাব এর ভিতর  if else  ব্যাবহার করতে পারি video--20.5****//
 
-function Rabbi(Name1, Name2){
-    const sum =Name1 + Name2;
-    const diff =Name1 - Name2;
-    const gun =sum * diff;
-    const Rakib =gun /2;
-    return Rakib;
-}
-Rabbi(20, 10)
- const Rayhan = Rabbi(20,10);
- console.log(Rayhan)
- const Rifat = Rabbi(50,25)
- console.log(Rifat)
+// function Rabbi(Name1, Name2){
+//     const sum =Name1 + Name2;
+//     const diff =Name1 - Name2;
+//     const gun =sum * diff;
+//     const Rakib =gun /2;
+//     return Rakib;
+// }
+// Rabbi(20, 10)
+//  const Rayhan = Rabbi(20,10);
+//  console.log(Rayhan)
+//  const Rifat = Rabbi(50,25)
+//  console.log(Rifat)
 
 // *******************************
  
 //  *** if বা else জোর বিজোড় খজার নিয়েম ***
 // জোড় বিষয় "০"/////
 
-function Rabbi(Nambar){
-    if(Nambar % 2===0){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-console.log(Rabbi(10))
-console.log(Rabbi(115))
+// function Rabbi(Nambar){
+//     if(Nambar % 2===0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// console.log(Rabbi(10))
+// console.log(Rabbi(115))
 
 // *******************************
 
 // বিজোড় বিষয় "১"////
 
-function Rayhan(Nambar){
-    if(Nambar % 2===1){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-console.log(Rayhan(115))
-console.log(Rayhan(120))
+// function Rayhan(Nambar){
+//     if(Nambar % 2===1){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// console.log(Rayhan(115))
+// console.log(Rayhan(120))
 
 // ----------------------------------------------------------------------------------------------------;
 
 //****** srting সটকাট বলে str এটা দ্বারা কাজ করে if else bisay ***video--20.6**//
 
-function Rabbi(str){
-    const size =str.length;
-    console.log(str,size)
-}
-Rabbi('Bangladesh');
-Rabbi('Dhaka');
+// function Rabbi(str){
+//     const size =str.length;
+//     console.log(str,size)
+// }
+// Rabbi('Bangladesh');
+// Rabbi('Dhaka');
 
 // ******************************;
 
-function Rakib(str){
-    const size=str.length;
-    console.log(str,size)
-    if(size % 2 ===0){
-        console.log('this is a heading.')
-        return true;
-    }
-    else {
-        console.log('This is a paragraph.')
-        return false;
-    }
-}
+// function Rakib(str){
+//     const size=str.length;
+//     console.log(str,size)
+//     if(size % 2 ===0){
+//         console.log('this is a heading.')
+//         return true;
+//     }
+//     else {
+//         console.log('This is a paragraph.')
+//         return false;
+//     }
+// }
 
 // ----------------------------------------------------------------------------------------------------;
 
 // ***** এরে কাজ নামবার নিয়ে কাজ যোগ করে এরে আনা *** video --20.7***///
 
-function Rabbi(nambar){
-    let sum =0;
-    for(const nam of nambar){
-        console.log(nam);
-        sum =sum +nam;
-    }
-    return sum;
-}
-const Nambar=[10, 20, 30, 40];
-const sum =Rabbi(Nambar);
-console.log('this is a heading', sum)
+// function Rabbi(nambar){
+//     let sum =0;
+//     for(const nam of nambar){
+//         console.log(nam);
+//         sum =sum +nam;
+//     }
+//     return sum;
+// }
+// const Nambar=[10, 20, 30, 40];
+// const sum =Rabbi(Nambar);
+// console.log('this is a heading', sum)
 
 // ******খুব গুরুত্বপূর্ণ বিষয় @@@@@@@@@@;
 
