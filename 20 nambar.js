8519d93de51b0ed6fd2c8a26f95ee47225ef676c
Rabbi44d <rb15968732@gmail.com>
2024-02-05 16:00:52 +0600

    20 nambar module imp

---
+++ 20 nambar.js
@@ -0,0 +1,9 @@
+
+// ফংশন বিষয় আলোচনা বর্গ করার নিয়ম বর্গ সুদু এক ভাবে করা যায় ***video--20-1,2***//
+
+function Rabbi(nambar){
+    console.log('This is a heading', nambar)
+    const borgo = nambar * nambar ;
+    console.log('this is a paragraph.', borgo)
+}
+Rabbi(4);
---
+++ Rabby.js
@@ -0,0 +1,9 @@
+function Rabbi(Name1 , Name2){
+    const Rakib =Name1+Name2;
+    const Rayhan =Name1+Name2;
+    const Rafi =Rakib*Rayhan;
+    const Rifat =Rafi /2;
+    return Rifat;
+}
+const Ra=Rabbi(10,5);
+console.log(Ra)
